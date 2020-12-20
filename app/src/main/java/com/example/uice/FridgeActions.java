e596aed11c00967bab139b69077e25c3eab98753
svtvrn <nikoschasapis99@gmail.com>
2020-12-20 17:09:48 +0200

    Gg

--- app/src/main/java/com/example/uice/FridgeActions.java
+++ app/src/main/java/com/example/uice/FridgeActions.java
@@ -2,13 +2,31 @@ package com.example.uice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class FridgeActions extends AppCompatActivity {
+
+    private ImageButton back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fridge_actions);
+
+        back = findViewById(R.id.fridge_actions_back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMainMenu();
+            }
+        });
+    }
+
+    public void openMainMenu(){
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
     }
 }
