5a98ebc15b873daf2cfbf6745256fa87fae44071
svtvrn <nikoschasapis99@gmail.com>
2020-12-27 19:02:44 +0200

    Gg

---
+++ app/src/main/java/com/example/uice/Board.java
@@ -0,0 +1,75 @@
+package com.example.uice;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.Nullable;
+import java.util.ArrayList;
+import static com.example.uice.FridgeNotes.brush;
+import static com.example.uice.FridgeNotes.path;
+
+public class Board extends View {
+
+    static ArrayList<Path> pathList = new ArrayList<>();
+    static ArrayList<Integer> colorList = new ArrayList<>();
+    ViewGroup.LayoutParams params;
+    static int currentBrush = R.id.black_colour;
+
+    public Board(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public Board(Context context, @Nullable AttributeSet attrs){
+        super(context,attrs);
+        init(context);
+    }
+
+    public Board(Context context, @Nullable AttributeSet attrs, int defStyleAttr){
+        super(context,attrs,defStyleAttr);
+        init(context);
+    }
+
+    void init (Context context){
+        brush.setAntiAlias(true);
+        brush.setColor(currentBrush);
+        brush.setStyle(Paint.Style.STROKE);
+        brush.setStrokeCap(Paint.Cap.ROUND);
+        brush.setStrokeJoin(Paint.Join.ROUND);
+        brush.setStrokeWidth(20f);
+
+        params = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        float x = event.getX();
+        float y = event.getY();
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                path.moveTo(x,y);
+                invalidate();
+            case MotionEvent.ACTION_MOVE:
+                path.lineTo(x,y);
+                pathList.add(path);
+                colorList.add(currentBrush);
+                invalidate();
+                return true;
+            default:
+                return false;
+        }
+    }
+    @Override
+    public void onDraw(Canvas canvas){
+        for(int i=0; i<pathList.size(); i++){
+            brush.setColor(colorList.get(i));
+            canvas.drawPath(pathList.get(i),brush);
+            invalidate();
+        }
+    }
+}
--- app/src/main/java/com/example/uice/FridgeNotes.java
+++ app/src/main/java/com/example/uice/FridgeNotes.java
@@ -2,17 +2,12 @@ package com.example.uice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
-import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
 
-import java.util.ArrayList;
 
 public class FridgeNotes extends AppCompatActivity {
 
@@ -30,20 +25,15 @@ public class FridgeNotes extends AppCompatActivity {
         currentColor(brush.getColor());
     }
     public void eraser(View view){
-        Board.pathList.clear();
-        Board.colorList.clear();
-        path.reset();
-    }
-    public void whiteColour(View view){
         brush.setColor(Color.WHITE);
         currentColor(brush.getColor());
     }
     public void blackColour(View view){
-        brush.setColor(Color.WHITE);
+        brush.setColor(Color.BLACK);
         currentColor(brush.getColor());
     }
     public void redColour(View view){
-        brush.setColor(Color.BLACK);
+        brush.setColor(Color.RED);
         currentColor(brush.getColor());
     }
     public void yellowColour(View view){
@@ -60,55 +50,9 @@ public class FridgeNotes extends AppCompatActivity {
         path = new Path();
     }
 
-
-     static class Board extends View {
-
-        static ArrayList<Path> pathList = new ArrayList<>();
-        static ArrayList<Integer> colorList = new ArrayList<>();
-        static ViewGroup.LayoutParams params;
-        static int currentBrush = R.id.black_colour;
-
-        public Board(Context context) {
-            super(context);
-            init(context);
-        }
-
-        void init (Context context){
-            brush.setAntiAlias(true);
-            brush.setColor(currentBrush);
-            brush.setStyle(Paint.Style.STROKE);
-            brush.setStrokeCap(Paint.Cap.ROUND);
-            brush.setStrokeJoin(Paint.Join.ROUND);
-            brush.setStrokeWidth(10f);
-
-            params = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
-        }
-
-        @Override
-        public boolean onTouchEvent(MotionEvent event){
-            float x = event.getX();
-            float y = event.getY();
-            switch (event.getAction()){
-                case MotionEvent.ACTION_DOWN:
-                    path.moveTo(x,y);
-                    invalidate();
-                case MotionEvent.ACTION_MOVE:
-                    path.lineTo(x,y);
-                    pathList.add(path);
-                    colorList.add(currentBrush);
-                    invalidate();
-                    return true;
-                default:
-                    return false;
-            }
-        }
-        @Override
-        public void onDraw(Canvas canvas){
-            for(int i=0; i<pathList.size(); i++){
-                brush.setColor(colorList.get(i));
-                canvas.drawPath(pathList.get(i),brush);
-                invalidate();
-            }
-        }
+    public void clear(View view) {
+        Board.pathList.clear();
+        Board.colorList.clear();
+        path.reset();
     }
 }
